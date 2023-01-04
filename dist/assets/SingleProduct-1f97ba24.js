a3f59e26f718a2f93e31bc087e418b821b8a489f
Ananas91Alsaadi <anas_al-saadi@hotmail.com>
2023-01-04 12:21:30 +0100

    disthem

---
+++ dist/assets/SingleProduct-1f97ba24.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o}from"./index-b3fb1776.js";const r={data(){return{}},methods:{}};function c(n,a,s,_,p,d){return t(),o("main")}const i=e(r,[["render",c]]);export{i as default};
